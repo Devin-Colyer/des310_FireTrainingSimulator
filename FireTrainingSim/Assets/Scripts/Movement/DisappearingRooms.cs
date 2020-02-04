9451f843e0562c1218815a76e6ae05bda3d41f6d
Devin <devcolyer@gmail.com>
2020-02-04 14:36:47 +0000

    Point & Click Movement Implemented
    Point and click movement controller script works. Room dissapearance does not work but is a wip. Scene with baked nav mesh made.

---
+++ FireTrainingSim/Assets/Scripts/Movement/DisappearingRooms.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearingRooms : MonoBehaviour {
+    
+    MeshRenderer[] m_ChildMeshRenderers;
+    // Use this for initialization
+    void Start()
+    {
+        MeshRenderer[] l_MeshRenderers = GetComponentsInChildren<MeshRenderer>();
+
+        if (l_MeshRenderers == null)
+            Debug.Log("No Mesh Renderers Found");
+        else
+        {
+            m_ChildMeshRenderers = l_MeshRenderers;
+
+            Debug.Log("Found " + m_ChildMeshRenderers.Length.ToString() + " Renderers");
+        }
+    }
+
+    private void OnCollisionEnter(Collision in_CollisionData)
+    {
+        if (in_CollisionData.collider.tag == "RenderRoomCollider")
+        {
+            Debug.Log("Render The Room");
+            foreach (MeshRenderer i in m_ChildMeshRenderers)
+            {
+                i.enabled = true;
+            }
+        }
+        else
+            return;
+    }
+    private void OnCollisionExit(Collision in_CollisionData)
+    {
+        if (in_CollisionData.collider.tag == "RenderRoomCollider")
+        {
+            Debug.Log("Stop Rendering The Room");
+            foreach (MeshRenderer i in m_ChildMeshRenderers)
+            {
+                i.enabled = false;
+            }
+        }
+        else
+            return;
+    }
+}
---
+++ FireTrainingSim/Assets/Scripts/Movement/PointAndClickPlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PointAndClickPlayerController : MonoBehaviour {
+
+    public Camera m_camera;
+    NavMeshAgent m_Agent;
+
+    // Use this for initialization
+    void Start () {
+        m_Agent = GetComponent<NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit l_Hit;
+            Ray l_Ray = m_camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(l_Ray, out l_Hit, 100))
+            {
+                m_Agent.destination = l_Hit.point;
+            }
+        }
+    }
+}
