c4aede10537054df8c7323bcf305e8604fa3598b
Michael Lindermanis <58297111+1701826@users.noreply.github.com>
2020-02-05 19:47:45 +0000

    General gameplay improvements, added win condition.
    Changes and Additions:
    - Made improvements to the way box pickup is handled. Objects are now moved around the point they are clicked rather than snapping the centre of the object to the mouse. This shoud make gameplay smoother.
    - Added a trigger around the door to detect blocking objects.
    - Game will now  show a message when all boxes are removed.
    - Placed other moveable objects around the scene. These cannot be used to block the door.
    - Added invisible wall in front of the table. This should stop boxes from goin out of the scene while still allowing full physics with the rest of the environment. Clutter objects are allowed to pass through the wall and go out of scene.
    
    Known Issues:
    - Some objects will still pass through geometry if they move quick enough. This should be fixable by enabling continuous collision detection.

---
+++ FireTrainingSim/Assets/Scripts/Box Minigame/BoxTriggerScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxTriggerScript : MonoBehaviour
+{
+    private int g_colliderCount;
+
+    // Use this for initialization
+    void Start()
+    {
+        g_colliderCount = 0;
+    }
+
+    // Called when an object enters the trigger volume.
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.parent)
+        {
+            if (other.transform.parent.name == "Door Blockables")
+            {
+                // Increment collider count.
+                g_colliderCount++;
+
+                // Debug output.
+                Debug.Log("An object is blocking the door.");
+            }
+        }
+    }
+
+    // Called when an object exits the trigger volume.
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.parent)
+        {
+            if (other.transform.parent.name == "Door Blockables")
+            {
+                // Decrease collider count.
+                g_colliderCount--;
+                
+                if (g_colliderCount == 0)
+                {
+                    // Find overlay.
+                    GameObject l_overlay = GameObject.Find("Overlay");
+
+                    if (l_overlay)
+                    {
+                        // Render text display.
+                        GameObject text = l_overlay.transform.Find("Game Complete Text").gameObject;
+                        text.SetActive(true);
+                    }
+
+                    // Debug output.
+                    Debug.Log("All object have been moved from the door.");
+                }
+                else
+                {
+                    // Debug output.
+                    Debug.Log("An object have been moved from the door.");
+                }
+            }
+        }
+    }
+}
---
+++ FireTrainingSim/Assets/Scripts/Box Minigame/InvisibleWallScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisibleWallScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        GameObject l_clutter = GameObject.Find("Moveable Clutter");
+
+        if (l_clutter)
+        {
+            // Debug output.
+            Debug.Log("Found moveable clutter.");
+
+            // Ignore collision with clutter objects.
+            foreach (Transform child in l_clutter.transform)
+            {
+                Physics.IgnoreCollision(child.GetComponent<Collider>(), this.GetComponent<Collider>(), true);
+
+                // Debug output.
+                Debug.Log("Ignored collision between " + child.name + " and " + this.name);
+            }
+        }
+    }
+}
