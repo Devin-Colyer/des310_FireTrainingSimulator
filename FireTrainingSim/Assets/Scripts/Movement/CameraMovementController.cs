3a41d7d916fc047458ee7e1a4ace9b8149b4d689
Devin <devcolyer@gmail.com>
2020-02-06 12:58:52 +0000

    Changed Names and Added Room Lips, Started on Camera Movement

---
+++ FireTrainingSim/Assets/Scripts/Movement/CameraMovementController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementController : MonoBehaviour {
+
+    Vector3 m_CamDestintaion;
+
+    // Use this for initialization
+    private void Start()
+    {
+        m_CamDestintaion = new Vector3(this.transform.position.x + 22, this.transform.position.y, this.transform.position.z + 22);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
