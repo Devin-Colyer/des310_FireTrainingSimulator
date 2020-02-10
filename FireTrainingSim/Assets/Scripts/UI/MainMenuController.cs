9e7c04e0031fd8fa4f9f95b0ec8bc396b474132b
Devin Colyer <1702010@uad.ac.uk>
2020-02-10 13:30:26 +0000

    Working Main Menu Added, Cleaned Up Files - Good Management

---
+++ FireTrainingSim/Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+    // When play button is pressed call this function
+    public void PlayButtonPressed()
+    {
+        // Move onto the next scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // When play button is pressed call this function
+    public void QuitButtonPressed()
+    {
+        // Debug
+        Debug.Log("Game has exited");
+
+        // Move onto the next scene
+        Application.Quit();
+    }
+}
