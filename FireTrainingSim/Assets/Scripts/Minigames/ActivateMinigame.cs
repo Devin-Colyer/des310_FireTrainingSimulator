27b855fcbdbce904458e705052c688cff0efd6c7
Devin <devcolyer@gmail.com>
2020-02-11 16:58:41 +0000

    Minigame Transfer Script

---
+++ FireTrainingSim/Assets/Scripts/Minigames/ActivateMinigame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateMinigame : MonoBehaviour {
+
+    public GameObject m_MinigameToActivate;
+    public Camera m_RoomCamera;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            m_MinigameToActivate.SetActive(true);
+            m_RoomCamera.enabled = false;
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ FireTrainingSim/Assets/Scripts/Minigames/SpaceToWin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceToWin : MonoBehaviour {
+
+    public Camera m_RoomCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_RoomCamera.enabled = true;
+            gameObject.SetActive(false);
+        }
+	}
+}
