69f5a4e1f928f7c60ac216d99da6a514b5215f6f
Michael Lindermanis <58297111+1701826@users.noreply.github.com>
2020-02-12 00:41:11 +0000

    Initial test for minigame functionality in the main level.
    Changes and Additions:
    - Added box minigame into main level.
    - Changing the camera to a minigame will now disable the level and all level related scripts before enabling the minigame. The opposite will happen when changing back to the level. This ensures that no scripts, such as the object grabber, are running when they should be.

---
+++ FireTrainingSim/Assets/Scripts/Movement/CameraSwitch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    // Stores the area assosciated with the camera.
+    public GameObject m_cameraArea;
+
+    private void OnEnable()
+    {
+        if(m_cameraArea)
+        {
+            // Enable camera area.
+            m_cameraArea.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (m_cameraArea)
+        {
+            // Disable camera area.
+            m_cameraArea.gameObject.SetActive(false);
+        }
+    }
+}
