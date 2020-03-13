f078e5aa9fb7f8d9c60f0275a28b6745bef1c545
ARTHUR COMPIN DUBUS <1702679@uad.ac.uk>
2020-03-13 17:00:29 +0000

    FirstPassToImplementSoundUpdate
    Footsteps mechanics

--- FireTrainingSim/Assets/Scripts/Sound/ThirdPersonSounds.cs
+++ FireTrainingSim/Assets/Scripts/Sound/ThirdPersonSounds.cs
@@ -8,28 +8,44 @@ public class ThirdPersonSounds : MonoBehaviour {
 
     // Wwise event to trigger a footstep sound.
     //public AK.Wwise.Event footstepSound = new AK.Wwise.Event();
- 
-	
-	// Check if Player move to play footstep sound
-	//void Update () {
- //       m_NavMeshAgent = GetComponent<NavMeshAgent>();
+    FMOD.Studio.EventInstance PlayFootstepsSound;
 
- //      if (m_NavMeshAgent.velocity.magnitude != 0)
- //       {
- //           //footstepSound.Post(gameObject);
- //           Debug.Log("bite");
- //       }
+    [FMODUnity.EventRef]
+    public string inputsound;
+
+    private void Start()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(inputsound);
+    }
+
+    //private void Awake()
+    //{
+    //    PlayFootstepsSound = FMODUnity.RuntimeManager.CreateInstance("event:");
+    //}
+
+
+    // Check if Player move to play footstep sound
+    //void Update () {
+    //       m_NavMeshAgent = GetComponent<NavMeshAgent>();
+
+    //      if (m_NavMeshAgent.velocity.magnitude != 0)
+    //       {
+    //           //footstepSound.Post(gameObject);
+    //           Debug.Log("bite");
+    //       }
 
 
 
 
- //   }
+    //   }
 
 
     // Called each time player animation timeline pass through "Footstep" event
     private void Footstep()
     {
         //footstepSound.Post(gameObject);
+        PlayFootstepsSound.start();
+        FMODUnity.RuntimeManager.PlayOneShot(inputsound);
         Debug.Log("step");
     }
 
