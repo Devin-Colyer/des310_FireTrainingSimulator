12741d939cb73f4b9545f0b447671aa5d0da75ed
ARTHUR COMPIN DUBUS <1702679@uad.ac.uk>
2020-03-15 17:53:50 +0000

    MusicSwitch Scripting

---
+++ FireTrainingSim/Assets/Scripts/Sound/MusicControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicControl : MonoBehaviour {
+
+    [FMODUnity.EventRef]
+    public string music = "event:/Music/Music";
+
+    FMOD.Studio.EventInstance musicEv;
+
+    void Start ()
+    {
+        musicEv = FMODUnity.RuntimeManager.CreateInstance(music);
+
+        musicEv.start();
+    }
+
+    public void GameStartedMusic()
+    {
+        musicEv.setParameterValue("Intro", 1f);
+    }
+
+    public void LoopMusic()
+    {
+        musicEv.setparameterValue("Loop", 1f);
+    }
+
+    public void outroMusic()
+    {
+        musicEv.setParameterValue("Outro", 1f);
+    }
+}
