d15653305c7513c6f5b7a58f7cf454f82c7e4046
Michael Lindermanis <58297111+1701826@users.noreply.github.com>
2020-04-13 19:06:37 +0100

    Restored changes from previous commit.
    Changes and Additions:
    - Updated materials in box minigame. Wall, table and shelf colours should now match what is seen in the level.

--- FireTrainingSim/Assets/Scripts/Movement/PointAndClickPlayerController.cs
+++ FireTrainingSim/Assets/Scripts/Movement/PointAndClickPlayerController.cs
@@ -27,11 +27,30 @@ public class PointAndClickPlayerController : MonoBehaviour {
             // Cast ray from the mouse
             if (Physics.Raycast(l_Ray, out l_Hit, 100))
             {
-                // Set the pathfinding destination to where the ray hits
-                m_Agent.destination = l_Hit.point;
+                // Check if collider exists.
+                if (l_Hit.collider)
+                {
+                    PointAndClickTrigger l_clickTrigger;
 
-                // Play walking sound
-                //m_Wwise.WalkPlay();
+                    // Check if collider is a click trigger.
+                    if (l_clickTrigger = l_Hit.collider.GetComponent<PointAndClickTrigger>())
+                    {
+                        // Move to trigger destination.
+                        m_Agent.destination = l_clickTrigger.GetDestination();
+                    }
+                    else
+                    {
+                        NavMeshHit l_navMeshHit;
+
+                        if (NavMesh.SamplePosition(l_Hit.point, out l_navMeshHit, 1.0f, NavMesh.AllAreas))
+                        {
+                            m_Agent.destination = l_navMeshHit.position;
+                        }
+
+                        // Set the pathfinding destination to where the ray hits
+                        //m_Agent.destination = l_Hit.point;
+                    }
+                }
             }
         }
     }
