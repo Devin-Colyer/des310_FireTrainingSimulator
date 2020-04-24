37dc5693d7a5245daf6d1d0458374933e88acf79
ArthurCD <32227254+ArthurCD@users.noreply.github.com>
2020-04-24 17:45:14 +0200

    Box Sound Environment Reacting (Will need help)
    2 of the 3 different "BoxSounds" are implemented, will need some help for the last one

--- FireTrainingSim/Assets/Scripts/Minigames/Box Minigame/BoxClickPickup.cs
+++ FireTrainingSim/Assets/Scripts/Minigames/Box Minigame/BoxClickPickup.cs
@@ -7,6 +7,9 @@ public class BoxClickPickup : MonoBehaviour
     GameObject g_grabbedObject;
     Vector3 g_grabOffset;
 
+    public GameObject Box_Sounds_Obj;
+    bool IsGrabbed=false;
+
 	// Use this for initialization
 	void Start ()
     {
@@ -62,6 +65,10 @@ public class BoxClickPickup : MonoBehaviour
 
                         // Debug, output object name.
                         Debug.Log("Object Hit: " + g_grabbedObject.name);
+
+                        // Play Pickup Sound
+                        Box_Sounds_Obj.GetComponent<BoxSounds>().F_BoxPickup();
+                        IsGrabbed = true;
                     }
                 }
             }
@@ -122,6 +129,13 @@ public class BoxClickPickup : MonoBehaviour
         {
             // Release grabbed object.
             DropGrabbedObject();
+            if (IsGrabbed)
+            {
+                // Play Drop Sound
+                Box_Sounds_Obj.GetComponent<BoxSounds>().F_BoxDrop();
+                IsGrabbed = false;
+                Debug.Log("Balance");
+            }
         }
     }
 
