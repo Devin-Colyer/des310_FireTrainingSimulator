924a7a24151ebc0184a3dfcc36383862ef39647f
Michael Lindermanis <58297111+1701826@users.noreply.github.com>
2020-04-27 08:25:35 +0100

    Updated section 3 blockout.
    Changes and Additions:
    - Added final room to section 3 blockout.
    - Updated models in kitchen minigame (section 3).
    - Created new material for tile floor. This should be more much more optimal than dividing a floor up into seperate coloured quads.

---
+++ FireTrainingSim/Assets/Editor/Screenshot.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class Screenshot : ScriptableObject
+{
+    [MenuItem("File/Take Screenshot %F12")]
+    static void TakeScreenshot()
+    {
+        string filename = "Screenshots/screenshot_" + System.DateTime.Now.ToString("yy-MM-dd_HH-mm-ss") + ".png";
+        ScreenCapture.CaptureScreenshot(filename, 2);
+    }
+}
